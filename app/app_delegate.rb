adc6743d78a7b8c2c0324775c780b4e1a0500be2
John Z Wu <bogardon@gmail.com>
2013-06-04 13:12:16 -0700

    [app_delegate] added some Facebook calls. set tab_bar_controller as rootvc.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,33 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
+
+    # Cache
+    cache = NSURLCache.alloc.initWithMemoryCapacity 4*1024*1024,
+      diskCapacity: 20*1024*1024,
+      diskPath: nil
+    NSURLCache.setSharedURLCache(cache)
+
+    @tab_bar_controller = TabBarController.alloc.init
+    vc = UIViewController.alloc.init
+    vc.view.backgroundColor = UIColor.blueColor
+    @tab_bar_controller.viewControllers = [vc]
+
+    @window = UIWindow.alloc.initWithFrame UIScreen.mainScreen.bounds
+    @window.rootViewController = @tab_bar_controller
+    @window.makeKeyAndVisible
+
     true
+  end
+
+  def application(application, openURL:url, sourceApplication:sourceApplication, annotation:annotation)
+    FBSession.activeSession.handleOpenURL url
+  end
+
+  def applicationDidBecomeActive(application)
+    FBSession.activeSession.handleDidBecomeActive
+  end
+
+  def applicationWillTerminate(application)
+    FBSession.activeSession.close
   end
 end
---
+++ app/view_controllers/tab_bar_controller.rb
@@ -0,0 +1,3 @@
+class TabBarController < UITabBarController
+
+end
