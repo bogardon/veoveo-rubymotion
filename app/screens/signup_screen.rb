7664e3ccc4f4535283d85986300a969721270fe6
John Z Wu <bogardon@gmail.com>
2013-06-04 19:45:32 -0700

    refactored some code

--- app/screens/signup_screen.rb
+++ app/screens/signup_screen.rb
@@ -2,10 +2,8 @@ class SignUpScreen < PM::Screen
   stylesheet :signup_screen
   layout do
     subview(UIImageView, :background)
-    scroll_view = subview(UIScrollView, :scrollview)
-    layout(scroll_view) do
-      container = subview(UIImageView, :signup_form_container)
-      layout(container) do
+    subview(UIScrollView, :scrollview) do
+      container = subview(UIImageView, :signup_form_container) do
         subview(UILabel, :prompt, text: "1. Pick a Username & Password")
       end
     end
