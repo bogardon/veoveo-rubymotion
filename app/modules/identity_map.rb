ba4542fb19efb86d90d5c41bbea4c55593d3b1fe
John Z Wu <bogardon@gmail.com>
2013-06-09 02:41:49 -0700

    [identity_map] define method to return a new user or an old but updated user given json.

--- app/modules/identity_map.rb
+++ app/modules/identity_map.rb
@@ -4,8 +4,20 @@ module VeoVeo
       base.extend(PublicClassMethods)
     end
     module PublicClassMethods
-      def merge_or_create
+      def merge_or_create(json)
+        existing_user = self.collection.find do |user|
+          user.id == json['id']
+        end
 
+        if existing_user
+          existing_user.columns.each do |column|
+            new_value = json[column.to_s]
+            existing_user.send("#{column.to_s}=", new_value) if new_value
+          end
+          return existing_user
+        else
+          return self.create json
+        end
       end
     end
   end
