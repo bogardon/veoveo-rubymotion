32f594b7054427fa248dfbfac0a99cf3365964ed
John Z Wu <bogardon@gmail.com>
2013-06-11 00:54:54 -0700

    [navigation_helpers] added method to add a right bar button

--- app/modules/navigation_helpers.rb
+++ app/modules/navigation_helpers.rb
@@ -2,4 +2,8 @@ module NavigationHelpers
   def add_logo_to_nav_bar
     self.navigationItem.titleView = UIImageView.alloc.initWithImage "logo.png".uiimage;
   end
+
+  def add_right_nav_button(title, target, selector)
+    self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithTitle(title, style:UIBarButtonItemStyleBordered, target:target, action:selector)
+  end
 end
