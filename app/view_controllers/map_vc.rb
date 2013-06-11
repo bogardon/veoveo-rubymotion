9f1c7bdfd7a4fdba0e495cd45b355f655fd03e99
John Z Wu <bogardon@gmail.com>
2013-06-11 00:57:08 -0700

    [map_vc] initial setup

--- app/view_controllers/map_vc.rb
+++ app/view_controllers/map_vc.rb
@@ -1,5 +1,6 @@
 class MapVC < UIViewController
   include NavigationHelpers
+
   stylesheet :map_vc
 
   layout do
@@ -9,8 +10,33 @@ class MapVC < UIViewController
     @map_view.userTrackingMode = MKUserTrackingModeFollow
   end
 
+  def init
+    super
+    @foreground_observer = App.notification_center.observe UIApplicationWillEnterForegroundNotification do |notification|
+
+    end
+    @background_observer = App.notification_center.observe UIApplicationDidEnterBackgroundNotification do |notification|
+    end
+    @login_observer = App.notification_center.observe CurrentUserDidLoginNotification do |notification|
+
+    end
+    self
+  end
+
+  def dealloc
+    App.notification_center.unobserve @foreground_observer
+    App.notification_center.unobserve @background_observer
+    App.notification_center.unobserve @login_observer
+  end
+
   def viewDidLoad
     super
     add_logo_to_nav_bar
+    add_right_nav_button "Logout", self, :logout
   end
+
+  def logout
+    User.current = nil
+  end
+
 end
