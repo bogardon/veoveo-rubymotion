2ab123eb0bc534c6c82a8aed3d41c8b06b5592fb
John Z Wu <bogardon@gmail.com>
2013-06-11 00:55:54 -0700

    [apperance_style] styled bar button title a little bit. the left/right padding needs to be fixed a little...

--- app/styles/appearance_style.rb
+++ app/styles/appearance_style.rb
@@ -18,6 +18,15 @@ Teacup.handler UIBarButtonItem, :back_bg_image_down { |image|
   setBackButtonBackgroundImage(image.uiimage.stretchable([16,14,16,4]), forState:UIControlStateNormal, barMetrics:UIBarMetricsDefault)
 }
 
+Teacup.handler UIBarButtonItem, :title_text_attributes { |attributes|
+  setTitleTextAttributes(attributes, forState:UIControlStateNormal)
+  setTitleTextAttributes(attributes, forState:UIControlStateHighlighted)
+}
+
+Teacup.handler UIBarButtonItem, :title_position_adjustment { |offset|
+  setTitlePositionAdjustment(offset, forBarMetrics:UIBarMetricsDefault)
+}
+
 Teacup::Appearance.new do
   style UINavigationBar,
     background: 'header.png'
@@ -25,5 +34,7 @@ Teacup::Appearance.new do
     bg_image_up: 'navbutton.png',
     bg_image_down: 'navbutton_down.png',
     back_bg_image_up: 'backbutton.png',
-    back_bg_image_down: 'backbutton_down.png'
+    back_bg_image_down: 'backbutton_down.png',
+    title_text_attributes: {UITextAttributeFont => UIFont.boldSystemFontOfSize(14)},
+    title_position_adjustment: [0,0]
 end
