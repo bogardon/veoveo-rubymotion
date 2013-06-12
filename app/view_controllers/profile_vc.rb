de52654ac869b6f89a0fa81d198bf8852fd76df8
John Z Wu <bogardon@gmail.com>
2013-06-11 17:08:57 -0700

    [profile_vc] add logout here.

--- app/view_controllers/profile_vc.rb
+++ app/view_controllers/profile_vc.rb
@@ -1,2 +1,12 @@
 class ProfileVC < UIViewController
+  include NavigationHelpers
+
+  def viewDidLoad
+    super
+    add_right_nav_button "Logout", self, :on_logout
+  end
+
+  def on_logout
+    User.current = nil
+  end
 end
