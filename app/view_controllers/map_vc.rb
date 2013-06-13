1c3dd8428f7c568193534f0dedfb8dbd650e74f5
John Z Wu <bogardon@gmail.com>
2013-06-12 17:13:11 -0700

    [map_vc] get spots based on region. and only add new spots as annotations.

--- app/view_controllers/map_vc.rb
+++ app/view_controllers/map_vc.rb
@@ -8,15 +8,12 @@ class MapVC < UIViewController
     @map_view.delegate = self
     @map_view.showsUserLocation = true
     @map_view.userTrackingMode = MKUserTrackingModeFollow
-    @map_view.addAnnotations(@spots)
   end
 
   def init
     super
     @foreground_observer = App.notification_center.observe UIApplicationWillEnterForegroundNotification do |notification|
-
-    end
-    @background_observer = App.notification_center.observe UIApplicationDidEnterBackgroundNotification do |notification|
+      reload
     end
     @login_observer = App.notification_center.observe CurrentUserDidLoginNotification do |notification|
       reload
@@ -29,24 +26,33 @@ class MapVC < UIViewController
 
   def dealloc
     App.notification_center.unobserve @foreground_observer
-    App.notification_center.unobserve @background_observer
     App.notification_center.unobserve @login_observer
   end
 
   def viewDidLoad
     super
     add_logo_to_nav_bar
-    add_right_nav_button "Logout", self, :logout
+    add_right_nav_button "Test", self, :on_test
   end
 
-  def logout
-    User.current = nil
+  def on_test
+    image = UIImagePNGRepresentation("bg.png".uiimage)
+    options = {
+      format: :form_data,
+      files: {image: image},
+      payload: {
+        latitude: 37.7858276367188,
+        longitude: -122.406402587891,
+        hint: "lalala"
+      }
+    }
+    VeoVeoAPI.post "spots", options do |response, json|
+
+    end
   end
 
   def mapView(mapView, didUpdateUserLocation:userLocation)
     return unless userLocation.location
-    p userLocation.location.coordinate.latitude
-    p userLocation.location.coordinate.longitude
   end
 
   SPOT_ANNOTATION_IDENTIFIER = "SPOT_ANNOTATION_IDENTIFIER"
@@ -57,21 +63,37 @@ class MapVC < UIViewController
     annotation
   end
 
-  def mapView(mapView, annotationView:annotation, calloutAccessoryTapped:control)
+  def mapView(mapView, annotationView:annotation, calloutAccessoryControlTapped:control)
+    p 'tapped'
+  end
 
+  def mapView(mapView, regionDidChangeAnimated:animated)
+    reload
   end
 
   def reload
-    return unless User.current
-    VeoVeoAPI.get 'spots' do |response, data|
-      if response.ok?
-        @spots = data.valueForKeyPath("spot").map do |json|
-          spot = Spot.merge_or_create json
-          spot_proxy = SpotProxy.alloc.init
-          spot_proxy.spot = spot
-          spot_proxy
+    return unless User.current && @map_view && @map_view.isUserLocationVisible
+
+    options = {
+      format: :json,
+      payload: {
+        region: {
+          latitude: @map_view.region.center.latitude,
+          longitude: @map_view.region.center.longitude,
+          latitude_delta: @map_view.region.span.latitudeDelta,
+          longitude_delta: @map_view.region.span.longitudeDelta
+        }
+      }
+    }
+    VeoVeoAPI.get 'spots', options do |response, spots|
+      if response.ok? && spots.present?
+        spot_proxies = spots.valueForKeyPath("spot").map do |spot|
+          proxy = SpotProxy.alloc.init
+          proxy.spot = Spot.merge_or_create spot
+          proxy
         end
-        @map_view.addAnnotations(@spots) if @map_view
+        new_spots = spot_proxies - @map_view.annotations
+        @map_view.addAnnotations(new_spots)
       end
     end
   end
