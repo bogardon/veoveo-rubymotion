755983faaef7c7295a4685477d3a0f2c883dbc00
John Z Wu <bogardon@gmail.com>
2013-06-14 13:48:18 -0700

    [spot_vc] implemented first cell, which looks for the same whether or not the spots unlocked.

--- app/view_controllers/spot_vc.rb
+++ app/view_controllers/spot_vc.rb
@@ -1,19 +1,83 @@
 class SpotVC < UIViewController
+  include NavigationHelpers
+
+  MAP_CELL_IDENTIFIER = "MAP_CELL_IDENTIFIER"
+  SPOT_ANNOTATION_IDENTIFIER = "SPOT_ANNOTATION_IDENTIFIER"
+
+  stylesheet :spot_vc
+
+  layout do
+    subview(UIImageView, :background)
+    @collection_view = subview(UICollectionView.alloc.initWithFrame(CGRectZero, collectionViewLayout:UICollectionViewFlowLayout.alloc.init), :collection_view, delegate:self, dataSource:self)
+    @collection_view.registerClass(MapCell, forCellWithReuseIdentifier:MAP_CELL_IDENTIFIER)
+  end
 
   attr_accessor :spot
 
   def initWithSpot(spot)
     init
     self.spot = spot
+    self.hidesBottomBarWhenPushed = true
     reload
     self
   end
 
+  def viewDidLoad
+    super
+    add_logo_to_nav_bar
+  end
 
   def reload
     Spot.for self.spot.id do |response, spot|
       self.spot = spot if response.ok?
+      @collection_view.reloadData if @collection_view
     end
+  end
+
+  def numberOfSectionsInCollectionView(collectionView)
+    1
+  end
+
+  def collectionView(collectionView, layout:collectionViewLayout, insetForSectionAtIndex:section)
+    [10,0,0,0]
+  end
+
+  def collectionView(collectionView, numberOfItemsInSection:section)
+    1
+  end
+
+  def collectionView(collectionView, layout:collectionViewLayout, sizeForItemAtIndexPath:indexPath)
+    [306,175]
+  end
+
+  def collectionView(collectionView, cellForItemAtIndexPath:indexPath)
+    cell = collectionView.dequeueReusableCellWithReuseIdentifier(MAP_CELL_IDENTIFIER, forIndexPath:indexPath)
+
+    spot_proxy = SpotProxy.alloc.init
+    spot_proxy.spot = @spot
+
+    cell.label.text = "\u201c#{spot_proxy.title}\u201d"
+    cell.map_view.region = MKCoordinateRegionMakeWithDistance(spot_proxy.coordinate, 100, 100)
+    cell.map_view.delegate = self
+    cell.map_view.setUserInteractionEnabled(false)
+
+    # don't re add annotations
+    cell.map_view.addAnnotation(spot_proxy) unless cell.map_view.annotations.count > 0
+    cell
+  end
+
+  def mapView(mapView, viewForAnnotation:annotation)
+    return nil unless annotation.isKindOfClass(SpotProxy)
+    cached = mapView.dequeueReusableAnnotationViewWithIdentifier(SPOT_ANNOTATION_IDENTIFIER)
+    annotation = cached || SpotAnnotationView.alloc.initWithAnnotation(annotation, reuseIdentifier:SPOT_ANNOTATION_IDENTIFIER)
+    annotation.canShowCallout = false
+    annotation.setSelected(true, animated:false)
+    annotation
+  end
+
+  def mapView(mapView, didUpdateUserLocation:userLocation)
+    return unless userLocation.location
+    @collection_view.reloadData if @collection_view
   end
 
 end
---
+++ app/views/map_cell.rb
@@ -0,0 +1,41 @@
+class MapCell < UICollectionViewCell
+
+  attr_accessor :map_view
+  attr_accessor :label
+
+  def initWithFrame(frame)
+    super
+
+
+    bot_bg = UIImageView.alloc.initWithImage "bottom_row.png".uiimage.stretchable([22,22,22,22])
+    bot_bg.frame = [[0, 45], [self.contentView.frame.size.width, self.contentView.frame.size.height-45]]
+    self.contentView.addSubview(bot_bg)
+
+    @map_view = MKMapView.alloc.initWithFrame([[1,45],[self.contentView.frame.size.width-2, self.contentView.frame.size.height - 45 - 1]])
+    @map_view.scrollEnabled = false
+    @map_view.zoomEnabled = false
+    @map_view.showsUserLocation = true
+
+    # round bottom
+    mask_layer = CAShapeLayer.layer
+    mask_layer.frame = @map_view.layer.bounds
+    path = UIBezierPath.bezierPathWithRoundedRect(mask_layer.bounds, byRoundingCorners:UIRectCornerBottomLeft|UIRectCornerBottomRight, cornerRadii:[3,3])
+    mask_layer.path = path.CGPath
+    @map_view.layer.mask = mask_layer
+    self.contentView.addSubview(@map_view)
+
+    top_bg = UIImageView.alloc.initWithImage "find_header.png".uiimage.stretchable([22,22,22,22])
+    top_bg.frame = [[0,0],[self.contentView.frame.size.width, 45]]
+    self.contentView.addSubview(top_bg)
+
+    @label = UILabel.alloc.initWithFrame(CGRectInset(top_bg.frame, 1, 1))
+    @label.backgroundColor = UIColor.clearColor
+    @label.textAlignment = NSTextAlignmentCenter
+    @label.font = UIFont.boldSystemFontOfSize(16)
+    @label.textColor = BubbleWrap.rgb_color(51,51,51)
+    self.contentView.addSubview(@label)
+
+    self
+  end
+
+end
