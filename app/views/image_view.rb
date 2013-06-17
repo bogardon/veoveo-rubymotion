0e15848399c667c4234636253268d47e4d386685
John Z Wu <bogardon@gmail.com>
2013-06-17 16:37:44 -0700

    [image_view] only do shit if response is ok.

--- app/views/image_view.rb
+++ app/views/image_view.rb
@@ -4,21 +4,26 @@ class ImageView < UIImageView
     image
   end
 
-
   def set_image_from_url(url)
     @query.connection.cancel if @query
+
     @query = BW::HTTP.get url.to_s do |response|
       @response = response
-      bg_queue = Dispatch::Queue.concurrent(priority=:default)
-      bg_queue.async do
-        image_from_data = UIImage.imageWithData(@response.body)
-        image_logical = UIImage.imageWithCGImage(image_from_data.CGImage, scale:UIScreen.mainScreen.scale, orientation:image_from_data.imageOrientation)
-        @processed_image = self.class.process_image image_logical
-        main_queue = Dispatch::Queue.main
-        main_queue.async do
-          self.image = @processed_image
+
+      if @response.ok?
+        bg_queue = Dispatch::Queue.concurrent(priority=:default)
+        bg_queue.async do
+          image_from_data = UIImage.imageWithData(@response.body)
+          image_logical = UIImage.imageWithCGImage(image_from_data.CGImage, scale:UIScreen.mainScreen.scale, orientation:image_from_data.imageOrientation)
+          @processed_image = self.class.process_image image_logical
+          main_queue = Dispatch::Queue.main
+          main_queue.async do
+            self.image = @processed_image
+          end
         end
+      else
       end
+
     end
   end
 
