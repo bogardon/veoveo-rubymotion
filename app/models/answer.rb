d5363e6033956a7eedf5a0f0a6ed748a259d2cbd
John Z Wu <bogardon@gmail.com>
2013-06-17 02:15:22 -0700

    [answer] added created_at field.

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,7 @@
 class Answer < Model
   set_attributes :image_url_small => :string,
-                 :image_url_large => :string
+                 :image_url_large => :string,
+                 :created_at => :date
 
   set_relationships :spot => :Spot,
                     :user => :User
