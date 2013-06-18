3b33251b42985ce130b134d530380b8ff3d4fd72
John Z Wu <bogardon@gmail.com>
2013-06-17 21:28:26 -0700

    [navigation_helpers] add method to create left button.

--- app/modules/navigation_helpers.rb
+++ app/modules/navigation_helpers.rb
@@ -4,6 +4,14 @@ module NavigationHelpers
   end
 
   def add_right_nav_button(title, target, selector)
-    self.navigationItem.rightBarButtonItem = UIBarButtonItem.alloc.initWithTitle(title, style:UIBarButtonItemStyleBordered, target:target, action:selector)
+    self.navigationItem.rightBarButtonItem = get_nav_button(title, target, selector)
+  end
+
+  def add_left_nav_button(title, target, selector)
+    self.navigationItem.leftBarButtonItem = get_nav_button(title, target, selector)
+  end
+
+  def get_nav_button(title, target, selector)
+    UIBarButtonItem.alloc.initWithTitle(title, style:UIBarButtonItemStyleBordered, target:target, action:selector)
   end
 end
