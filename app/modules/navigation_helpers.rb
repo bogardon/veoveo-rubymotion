5594dbccc4b11de9c8417987548d1dac6de3f234
John Z Wu <bogardon@gmail.com>
2013-06-17 23:22:29 -0700

    [navigation_helpers] add method to add title to navigationItem

--- app/modules/navigation_helpers.rb
+++ app/modules/navigation_helpers.rb
@@ -11,6 +11,18 @@ module NavigationHelpers
     self.navigationItem.leftBarButtonItem = get_nav_button(title, target, selector)
   end
 
+  def add_title_to_nav_bar(title)
+    label = UILabel.alloc.initWithFrame [[0,0],[320,44]]
+    label.backgroundColor = UIColor.clearColor
+    label.textAlignment = NSTextAlignmentCenter
+    label.textColor = UIColor.whiteColor
+    label.font = UIFont.boldSystemFontOfSize(20)
+    label.text = title
+    label.numberOfLines = 0
+    label.sizeToFit
+    self.navigationItem.titleView = label
+  end
+
   def get_nav_button(title, target, selector)
     UIBarButtonItem.alloc.initWithTitle(title, style:UIBarButtonItemStyleBordered, target:target, action:selector)
   end
