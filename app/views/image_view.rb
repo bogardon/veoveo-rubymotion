467076dd561d7defa4b015500eaf6b40f78e3f83
John Z Wu <bogardon@gmail.com>
2013-07-20 19:54:19 -0700

    [image_view] cache images and use RM-3 fix

--- app/views/image_view.rb
+++ app/views/image_view.rb
@@ -1,5 +1,9 @@
 class ImageView < UIImageView
 
+  def self.cache
+    @@cache ||= Hash.new
+  end
+
   def dealloc
     @query.connection.cancel if @query
     super
@@ -15,24 +19,27 @@ class ImageView < UIImageView
     return unless url.scheme
     @query.connection.cancel if @query
 
-    @query = BW::HTTP.get url.to_s do |response|
-      @response = response
-
-      if @response.ok?
-        bg_queue = Dispatch::Queue.concurrent(priority=:default)
-        bg_queue.async do
-          image_from_data = UIImage.imageWithData(@response.body)
-          image_logical = UIImage.imageWithCGImage(image_from_data.CGImage, scale:UIScreen.mainScreen.scale, orientation:image_from_data.imageOrientation)
-          @processed_image = block ? block.call(image_logical) : image_logical
-          main_queue = Dispatch::Queue.main
-          main_queue.async do
-            self.image = @processed_image
+    if cached = ImageView.cache.objectForKey(url.to_s)
+      self.image = cached
+    else
+      @query = BW::HTTP.get url.to_s do |response|
+        if response.ok?
+          bg_queue = Dispatch::Queue.concurrent(priority=:background)
+          bg_queue.async do
+            image_from_data = UIImage.imageWithData(response.body)
+            image_logical = UIImage.imageWithCGImage(image_from_data.CGImage, scale:UIScreen.mainScreen.scale, orientation:image_from_data.imageOrientation)
+            processed_image = block ? block.call(image_logical) : image_logical
+            ImageView.cache.setObject(processed_image, forKey:url.to_s)
+            main_queue = Dispatch::Queue.main
+            main_queue.async do
+              self.image = processed_image
+            end
           end
+        else
         end
-      else
       end
-
     end
+
   end
 
 end
