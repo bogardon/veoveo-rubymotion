2e10fbd047a047f165590871d03acb99d7e9184b
John Z Wu <bogardon@gmail.com>
2013-08-23 12:10:11 -0700

    shitty dividers, just to test.

--- app/views/map_segmented_control.rb
+++ app/views/map_segmented_control.rb
@@ -4,8 +4,11 @@ class MapSegmentedControl < UISegmentedControl
     super
     self.setBackgroundImage("navbutton.png".uiimage.stretchable([16,5,15,5]), forState:UIControlStateNormal, barMetrics:UIBarMetricsDefault)
     self.setBackgroundImage("navbutton_down.png".uiimage.stretchable([16,5,15,5]), forState:UIControlStateSelected, barMetrics:UIBarMetricsDefault)
-    self.setDividerImage(UIImage.alloc.init, forLeftSegmentState:UIControlStateNormal, rightSegmentState:UIControlStateSelected, barMetrics:UIBarMetricsDefault)
-    self.setDividerImage(UIImage.alloc.init, forLeftSegmentState:UIControlStateSelected, rightSegmentState:UIControlStateNormal, barMetrics:UIBarMetricsDefault)
+
+
+    self.setDividerImage("divider_up_down.png".uiimage, forLeftSegmentState:UIControlStateNormal, rightSegmentState:UIControlStateSelected, barMetrics:UIBarMetricsDefault)
+    self.setDividerImage("divider_down_up".uiimage, forLeftSegmentState:UIControlStateSelected, rightSegmentState:UIControlStateNormal, barMetrics:UIBarMetricsDefault)
+
     self.setDividerImage(UIImage.alloc.init, forLeftSegmentState:UIControlStateNormal, rightSegmentState:UIControlStateNormal, barMetrics:UIBarMetricsDefault)
     self.setDividerImage(UIImage.alloc.init, forLeftSegmentState:UIControlStateSelected, rightSegmentState:UIControlStateSelected, barMetrics:UIBarMetricsDefault)
 
