0d7eae6f33bf92485b7a48241470dfa7bebd77b5
John Z Wu <bogardon@gmail.com>
2013-08-23 13:46:18 -0700

    [image_view] don't do anything unless image returned with response OK!

--- app/views/image_view.rb
+++ app/views/image_view.rb
@@ -18,12 +18,14 @@ class ImageView < UIImageView
           self.image = object if @url.to_s == key
         else
           BW::HTTP.get url.to_s do |response|
-            Dispatch::Queue.concurrent(priority=:background).async do
-              image = UIImage.alloc.initWithData(response.body, scale:UIScreen.mainScreen.scale)
-              processed_image = block ? block.call(image) : image
-              TMCache.sharedCache.setObject(processed_image, forKey:key)
-              Dispatch::Queue.main.async do
-                self.image = processed_image if @url.to_s == key
+            if response.ok?
+              Dispatch::Queue.concurrent(priority=:background).async do
+                image = UIImage.alloc.initWithData(response.body, scale:UIScreen.mainScreen.scale)
+                processed_image = block ? block.call(image) : image
+                TMCache.sharedCache.setObject(processed_image, forKey:key)
+                Dispatch::Queue.main.async do
+                  self.image = processed_image if @url.to_s == key
+                end
               end
             end
           end
