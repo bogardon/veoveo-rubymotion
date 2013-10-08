9e2bd8bca852de28a07684a968b6127c0b237436
John Z Wu <bogardon@gmail.com>
2013-10-08 10:21:45 -0700

    [header_cell] this should not be a uicollectionviewcell.

--- app/views/header_cell.rb
+++ app/views/header_cell.rb
@@ -1,4 +1,4 @@
-class HeaderCell < UICollectionViewCell
+class HeaderCell < UICollectionReusableView
   attr_accessor :label
 
   def initWithFrame(frame)
@@ -6,7 +6,7 @@ class HeaderCell < UICollectionViewCell
     @label = UILabel.alloc.initWithFrame([[10,10],[200,20]])
     @label.backgroundColor = UIColor.clearColor
     @label.font = UIFont.boldSystemFontOfSize 14
-    self.contentView.addSubview(@label)
+    self.addSubview(@label)
     self
   end
 end
